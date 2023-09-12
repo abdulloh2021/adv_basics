899764d344f1dc612960ea4194d30830c301cf1c
Abdul 2014 <abdulloh.c2021@gmail.com>
2023-09-12 11:42:52 +0700

    edit star screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,8 +6,35 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Center(
-        child: Column(
-      children: [Image.asset('assets/images/quiz-logo.png')],
-    ));
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
   }
 }
